2c1844aabe37140fbf084f11afe9ca5eeee1d1b7
Billy Richardson <billy7373731@outlook.com>
2023-05-23 09:48:50 +0100

    sample item made in test

---
+++ server/models/items.test.js
@@ -0,0 +1,29 @@
+const { sequelize, Item } = require('./path/to/your/ItemModel.js');
+
+// Test case for creating an item
+describe('Item model', () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true }); // Drops and recreates the table before running the tests
+  });
+
+  afterAll(async () => {
+    await sequelize.close(); // Closes the database connection after running the tests
+  });
+  it('should create a new item', async () => {
+    const newItem = {
+      title: 'Test Item',
+      price: '10.00',
+      image: 'Img.com',
+      category: 'Sample Cat',
+      description: 'Sample Desc',
+    };
+
+    const test = await Item.create(newItem);
+
+    
+
+
+
+
+  })
+});
